a02c096cf833f54c393de4e8644d0f95e446d6e3
Juan.Ferreira <juan.ferreira@impar.com.br>
2024-08-12 16:55:49 -0300

    deploy

--- src/config/connect.py
+++ src/config/connect.py
@@ -2,7 +2,9 @@ import os
 import yaml
 
 def initialize():
-  with open('config.yaml', 'r') as config_file:
-      config = yaml.safe_load(config_file)
-  os.environ['OPENAI_API_KEY'] = config['OPENAI_API_KEY']
-  os.environ['PINECONE_API_KEY'] = config['PINECONE_API_KEY']
+  os.environ['OPENAI_API_KEY']
+  os.environ['PINECONE_API_KEY']
+  # with open('config.yaml', 'r') as config_file:
+  #     config = yaml.safe_load(config_file)
+  # os.environ['OPENAI_API_KEY'] = config['OPENAI_API_KEY']
+  # os.environ['PINECONE_API_KEY'] = config['PINECONE_API_KEY']
